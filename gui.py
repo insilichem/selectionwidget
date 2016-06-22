06e9fb11c836ff75f74af55b5bb8ff2d3761bf3e
Jaime RG <jaime.rogue@gmail.com>
2016-06-22 12:26:13 +0200

    Handle onscreen selections and better color palette

--- gui.py
+++ gui.py
@@ -7,14 +7,14 @@ from __future__ import print_function, division
 import Tkinter as tk
 import string
 import re
+from itertools import cycle
 # Chimera stuff
 import chimera
 from chimera.baseDialog import ModelessDialog
 from chimera.specifier import evalSpec
 from chimera.selection import (clearCurrent as clear_selection, addCurrent as add_to_current_selection,
                                currentContents as current_selection_contents)
-from chimera.colorTable import (colors as chimera_colors, getTkColorByName as tk_color,
-                                getColorByName as chimera_color)
+from chimera.colorTable import (getTkColorByName as tk_color, getColorByName as chimera_color)
 from Midas import focus
 # Additional 3rd parties
 
@@ -73,12 +73,8 @@ class SelectionDialog(ModelessDialog):
     normal_keys = string.letters + string.digits + '@:./-;,!?_'
     allowed_modes = ('atoms', 'bonds', 'residues', 'chains', 'molecules')
     white = chimera.MaterialColor.lookup('white')
-    ok_colors = ['blue', 'brown', 'dark cyan', 'dark green', 'dark grey', 
-                 'dark magenta', 'dark olive green', 'dark red', 'dark slate blue',
-                 'dark slate gray', 'dark slate grey', 'dim gray', 'dim grey', 
-                 'firebrick', 'forest green', 'medium blue', 'navy blue', 
-                 'orange red', 'purple', 'red', 'sea green', 'sienna', 
-                 'slate gray', 'slate grey', 'spring green', 'turquoise', 'violet red']
+    white.opacity = 0.5
+    ok_colors = ['blue', 'red', 'purple', 'sienna', 'slate grey', 'green', 'turquoise', 'gold']
 
     def __init__(self, parent=None, mode='atoms'):
         # Check
@@ -90,8 +86,13 @@ class SelectionDialog(ModelessDialog):
 
         # Private vars
         self._re = re.compile(r'(\s)')
-        self._old_selection = current_selection_contents()
+        self._old_selection = self.current()
         self._colored_molecules = []
+        self._depicted = []
+        self._spec_to_item = {}
+        self._item_to_spec = {}
+        self._parsed_queries = []
+        self._ok_tags_iter = cycle(iter(self.ok_colors))
 
         # Fire up
         ModelessDialog.__init__(self)
@@ -102,10 +103,11 @@ class SelectionDialog(ModelessDialog):
         self._fix_styles(*self.buttonWidgets.values())
 
         # Triggers
-        self._handlers = {}
-        self._handlers[('Model', self.desaturate_models)] = \
-            chimera.triggers.addHandler('Model', self.desaturate_models, None)
+        chimera.viewer.background = chimera.MaterialColor.lookup('white')
         self.desaturate_models()
+        self._handlers = {}
+        self._handlers[('file open', self.desaturate_models)] =  chimera.triggers.addHandler('file open', self.desaturate_models, None)
+        self._handlers[('selection changed', self.on_selection_changed)] =  chimera.triggers.addHandler('selection changed', self.on_selection_changed, None)
 
     def _initialPositionCheck(self, *args):
         try:
@@ -127,9 +129,9 @@ class SelectionDialog(ModelessDialog):
         the whole dialog, buttons, textareas and everything.
         """
         self.canvas = tk.Frame(parent)
-        self.canvas.pack()
+        self.canvas.pack(expand=True, fill='x')
         self.entry = tk.Text(self.canvas, width=50, height=1, **STYLES[tk.Text])
-        self.entry.pack(padx=10, pady=10)
+        self.entry.pack(padx=10, pady=10, expand=True, fill='x')
         self.entry.bind("<KeyRelease>", self.parse)
         self.entry.tag_config('wrong', background='red', foreground='white')
         
@@ -146,6 +148,7 @@ class SelectionDialog(ModelessDialog):
         """
         Default! Triggered action if you click on the Close button
         """
+        chimera.viewer.background = None
         self.resaturate_models()
         for (trigger, key), handler in self._handlers.items():
             chimera.triggers.deleteHandler(trigger, handler)
@@ -155,37 +158,87 @@ class SelectionDialog(ModelessDialog):
         self.destroy()
 
     # Custom methods
-    def parse(self, event):
-        if event.keysym in self.special_keys:
+    def parse(self, event=None):
+        if event and event.keysym in self.special_keys:
             return
-        content = event.widget.get(1.0, 'end-1c').strip('\n')
+        content = self.entry.get(1.0, 'end-1c').strip('\n')
         if not content:
             return
         # Save cursor position
         cursor = self.entry.index('insert')
-        queries = [q for q in self._re.split(content) if q]
+        
         # Clear before reformatting
         self.entry.delete(1.0, 'end')
         clear_selection()
+        self.undo_depict()
+        self._spec_to_item.clear()
+        self._item_to_spec.clear()
+        self._parsed_queries = []
 
-        ok_tags = iter(self.ok_colors)
+        self._ok_tags_iter = cycle(iter(self.ok_colors))
+        queries = [q for q in self._re.split(content) if q]
         for query, sep in map(None, queries[::2], queries[1::2]):
-            validated = self.validate(query)
-            if validated is not None:
-                tag = next(ok_tags)
-                self.depict(validated, color=tag)
+            sep = sep if sep else ''
+            item = self.validate(query)
+            if item is not None:
+                tag = next(self._ok_tags_iter)
+                self.depict(item, color=tag)
+                self._spec_to_item[query] = item
+                self._item_to_spec[item] = query
             else:
                 tag = 'wrong'
-            self.entry.insert('insert', query + (sep if sep else ''), tag)
+            self._parsed_queries.append([query, sep, item])
+            self.entry.insert('insert', query + sep, tag)
         # Restore cursor position
         self.entry.mark_set('insert', cursor)
-        current_selection = current_selection_contents()
+        current_selection = self.current()
         if self._old_selection != current_selection:
             self._old_selection = current_selection
             try:
                 self.focus()
             except:
                 pass
+
+    def on_selection_changed(self, *args):
+        cursor = self.entry.index('insert')
+        current = self.current()
+        added = set([i for i in current if i not in self._old_selection])
+        removed = set([i for i in self._old_selection if i not in current])
+        self._old_selection = current
+        self.entry.delete(1.0, 'end')
+        self._ok_tags_iter = cycle(iter(self.ok_colors))
+        self.undo_depict()
+        _parsed_queries_ = []
+        print(added)
+        print(removed)
+        for spec, sep, item in self._parsed_queries:
+            tag = 'wrong'
+            if item not in removed:
+                if item:
+                    tag = next(self._ok_tags_iter) 
+                    self.depict(item, color=tag, select=False)
+                self.entry.insert('insert', spec + sep, tag)
+                _parsed_queries_.append([spec, sep, item])
+        print(_parsed_queries_)
+        for item in added:
+            tag = next(self._ok_tags_iter)
+            spec = self._item_to_spec.get(item, self.get_spec(item))
+            self.entry.insert('insert', spec + ' ', tag)
+            self.depict(item, color=tag, select=False)
+            _parsed_queries_.append([spec, ' ', item])
+        self.entry.mark_set('insert', cursor)
+        self._parsed_queries = _parsed_queries_
+        print(_parsed_queries_)
+
+    def get_spec(self, item):
+        if self.mode == 'atoms':
+            return '#{}:{}.{}@{}'.format(item.molecule.id, item.residue.id.position, item.residue.id.chainId, item.name)
+        elif self.mode == 'molecules':
+            return '#{}'.format(item.molecule)
+        elif self.mode == 'residues':
+            return '#{}:{}.{}'.format(item.molecule.id, item.id.position, item.id.chainId)
+        elif self.mode == 'bonds':
+            pass
 
     def validate(self, query):
         try:
@@ -198,16 +251,22 @@ class SelectionDialog(ModelessDialog):
                 if len(current) == 1:
                     return current[0]
     
-    def depict(self, item, color=None):
+    def depict(self, item, color=None, select=True):
         atoms = [item] if self.mode == 'atoms' else item.atoms
+        if select:
+            self.untriggered_select(atoms)
+        self._depicted.extend(atoms)
         for a in atoms:
-            add_to_current_selection(a)
             mcolor = chimera_color(color)
-            print(color, mcolor)
             a.color = mcolor
-    
+
+    def undo_depict(self):
+        for a in self._depicted:
+            a.color = self.white
+        self._depicted = []
+
     def focus(self):
-        selected = getattr(chimera.selection, 'current' + self.mode.title())()
+        selected = self.current()
         if selected:
             focus('sel zr < 3')
             for item in selected:
@@ -221,21 +280,20 @@ class SelectionDialog(ModelessDialog):
                         a.display = True
         else:
             chimera.runCommand('focus')
-            self._colored_molecules = []
-            self.desaturate_models()
+            
 
     def desaturate_models(self, *args):
         for mol in chimera.openModels.list(modelTypes=[chimera.Molecule]):
             if mol in self._colored_molecules:
                 continue
             mol._old_color = mol.color
-            mol.color = None
+            mol.color = self.white
             for a in mol.atoms:
                 a._old_color = a.color
                 a.color = self.white
             for r in mol.residues:
                 r._old_colors = r.ribbonColor, r.fillColor
-                r.color = None
+                r.color = self.white
             self._colored_molecules.append(mol)
 
     def resaturate_models(self):
@@ -248,3 +306,11 @@ class SelectionDialog(ModelessDialog):
             for r in mol.residues:
                 r.ribbonColor, r.fillColor = r._old_colors
                 del r._old_colors
+
+    def current(self):
+        return getattr(chimera.selection, 'current' + self.mode.title())()
+
+    def untriggered_select(self, items):
+        chimera.triggers.blockTrigger('selection changed')
+        add_to_current_selection(items)
+        chimera.triggers.releaseTrigger('selection changed')
